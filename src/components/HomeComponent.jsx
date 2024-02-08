b64e8ae119fa457d5ff5b8c50193facafc973579
Yogesh Kumar <yogeshkumar.pc00@gmail.com>
2024-02-08 14:12:06 +0530

    Designed Topbar and install some packages such as icons

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Topbar from "./common/Topbar/Topbar";
 export default function HomeComponent() {
   return <div>HomeComponent</div>;
 }
--- src/components/common/Topbar/Topbar.jsx
+++ src/components/common/Topbar/Topbar.jsx
@@ -1,5 +1,48 @@
 import React from "react";
+import LinkedinLogo from "../../../assets/linkedinLogo.png";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiOutlineSearch,
+  AiOutlineBell,
+  AiOutlineMessage,
+} from "react-icons/ai";
+import { BsBriefcase } from "react-icons/bs";
+import User from "../../../assets/user.png";
 
 export default function Toobar() {
-  return <div>Topbar</div>;
+  return (
+    <>
+      <div className="w-full h-16 bg-white flex justify-between items-center p-6">
+        <img src={LinkedinLogo} alt="LinkedinLogo" className="h-10 w-12" />
+        <div className="flex flex-row justify-around w-full">
+          <AiOutlineSearch
+            size={25}
+            className="cursor-pointer text-[#6f6e6e] hover:text-black"
+          />
+          <AiOutlineHome
+            size={25}
+            className="cursor-pointer text-[#6f6e6e] hover:text-black"
+          />
+          <AiOutlineUser
+            size={25}
+            className="cursor-pointer text-[#6f6e6e] hover:text-black"
+          />
+          <BsBriefcase
+            size={25}
+            className="cursor-pointer text-[#6f6e6e] hover:text-black"
+          />
+          <AiOutlineBell
+            size={25}
+            className="cursor-pointer text-[#6f6e6e] hover:text-black"
+          />
+          <AiOutlineMessage
+            size={25}
+            className="cursor-pointer text-[#6f6e6e] hover:text-black"
+          />
+        </div>
+        <img src={User} alt="User image" className="w-8" />
+      </div>
+    </>
+  );
 }
